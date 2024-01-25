0438d08f96b2b69054898c1c2261da013694e713
rinij <rinimani7@gmail.com>
2024-01-24 23:06:28 -0700

    components rendered in App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-
-import React, { Component } from 'react';
-import Header from './components/Header';
-import About from './components/About';
-import Resume from './components/Resume';
-import Portfolio from './components/Portfolio';
-import Testimonials from  './components/Testimonials';
-import ContactUs from './components/ContactUs';
-import Footer from './components/Footer';
-import resumeData from './resumeData';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./components/Header";
+import About from "./components/About";
+import Resume from "./components/Resume";
+import Portfolio from "./components/Portfolio";
+import Testimonials from "./components/Testimonials";
+import ContactUs from "./components/ContactUs";
+import Footer from "./components/Footer";
+// import resumeData from "./resumeData";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+      <Header />
+      <About />
+      <Resume />
+      <Portfolio />
+      <Testimonials />
+      <ContactUs />
+      <Footer />
     </div>
   );
 }

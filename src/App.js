d02508f5e3842e09a5f04c25bef346d3581f3d25
rinij <rinimani7@gmail.com>
2024-01-24 23:04:00 -0700

    components declared & created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
-import logo from './logo.svg';
+
+import React, { Component } from 'react';
+import Header from './components/Header';
+import About from './components/About';
+import Resume from './components/Resume';
+import Portfolio from './components/Portfolio';
+import Testimonials from  './components/Testimonials';
+import ContactUs from './components/ContactUs';
+import Footer from './components/Footer';
+import resumeData from './resumeData';
 import './App.css';
 
 function App() {

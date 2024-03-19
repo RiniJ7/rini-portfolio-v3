da6386a539911093643cb86cebff0b1ef3244ad6
rinij <rinimani7@gmail.com>
2024-03-18 21:04:11 -0600

    text color modified

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -14,11 +14,11 @@ const Main = () => {
       <div className='w-full h-screen absolute top-0 left-0'>
         <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
           <div className='w-full p-4  bg-black/40'>
-            <h1 className='sm:text-5xl text-4xl font-bold text-blue-500'>
+            <h1 className='sm:text-5xl text-4xl font-bold text-white-500'>
               Hello! I'm Rini Joy
             </h1>
 
-            <h2 className='flex sm:text-3xl text-2xl pt-4 text-sky-300'>
+            <h2 className='flex sm:text-3xl text-2xl pt-4 text-white-300'>
               I'm a
               <TypeAnimation
                 sequence={[
@@ -39,7 +39,7 @@ const Main = () => {
               />
             </h2>
 
-            <div className='flex justify-between pt-6 max-w-[200px] w-full text-blue-500'>
+            <div className='flex justify-between pt-6 max-w-[200px] w-full text-white-500'>
               <FaLinkedinIn
                 className='cursor-pointer'
                 onClick={() => {

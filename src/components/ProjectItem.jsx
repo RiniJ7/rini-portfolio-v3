b7acd687b5593cb0f4fcd160e3e702fec6f71dd6
rinij <rinimani7@gmail.com>
2024-04-04 11:16:03 -0600

    added projects section

---
+++ src/components/ProjectItem.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { AiFillGithub } from "react-icons/ai";
+import { RiPagesLine } from "react-icons/ri";
+
+const ProjectItem = ({ img, title, projgithub, projlink }) => {
+  return (
+    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]">
+      <img
+        src={img}
+        alt="/"
+        className="rounded-xl group-hover:opacity-10 object-cover h-full"
+      />
+      <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+        <h3 className="text-2xl font-bold text-white tracking-wider text-center">
+          {title}
+        </h3>
+        {/* <a href={link}>
+          <p className="text-center p-2 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-lg">
+            More info
+          </p>
+        </a> */}
+
+        <div className="flex p-4 justify-center gap-20 flex-wrap">
+          <a
+            className="text-white font-bold hover:scale-105 transition-all duration-300"
+            href={projgithub}
+          >
+            <AiFillGithub size={32} />
+          </a>
+
+          {projlink ? (
+            <a
+              className=" text-white font-bold hover:scale-105 transition-all duration-300"
+              href={projlink}
+            >
+              <RiPagesLine size={32} />
+            </a>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectItem;
---
+++ src/components/Projects.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import ProjectItem from "./ProjectItem";
+import ProjectHead from "./ProjectHead";
+
+
+const Projects = () => {
+  return (
+    <div id="projects" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+      <h1 className="text-4xl font-bold text-center text-[#001b5e]">
+        Projects
+      </h1>
+      <div>
+        <ProjectHead />
+      </div>
+      <div className="grid sm:grid-cols-2 gap-12">
+        {/* php */}
+        <ProjectItem
+        //   img={phpproj}
+          title="E-commerce web with PHP and MySQL"
+          projgithub="https://github.com/yanliu1111/php-tutorial/tree/main/14_product_crud/03_mvc"
+        />
+        {/* echarts */}
+        <ProjectItem
+        //   img={echarts}
+          title="Realtime pandemic dashboard using web scrapping data"
+          projgithub="https://github.com/yanliu1111/dashboard-flask-echarts"
+        />
+        {/* prisma */}
+        <ProjectItem
+        //   img={prisma}
+          title="Turbo monorepo project with ORMs and data migrations strategies"
+          projgithub="https://github.com/yanliu1111/monorepo-docker-fullstack-app"
+        />
+        {/* inventory */}
+        <ProjectItem
+        //   img={inventory}
+          title="Inventory and Payment App & Microservices"
+          projgithub="https://github.com/yanliu1111/microservices-fastapi-fullStack-app"
+        />
+        {/* embeddings */}
+        <ProjectItem
+        //   img={embeddings}
+          title="ChatGPT API Embeddings Supabase"
+          projgithub="https://github.com/yanliu1111/ChatGPT-API-embeddings-Supabase"
+        />
+        {/* flaskrestapi */}
+        <ProjectItem
+        //   img={flaskrestapi}
+          title="Build a REST API with Flask"
+          projgithub="https://github.com/yanliu1111/flask-rest-api-project"
+          projlink="https://flask-rest-api-project-rmrw.onrender.com/"
+        />
+ 
+      
+      </div>
+    </div>
+  );
+};
+
+export default Projects;

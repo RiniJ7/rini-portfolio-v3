148bfb58512cd1b67d9501a3aa9d0b388ab35c53
rinij <rinimani7@gmail.com>
2024-04-04 11:15:33 -0600

    side navigation modified

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -48,7 +48,7 @@ const Sidenav = () => {
           </a>
           <a
             onClick={handleNav}
-            href="#main"
+            href="#about"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray
             -100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
@@ -89,7 +89,7 @@ const Sidenav = () => {
             <AiOutlineProject size={20} />
           </a>
           <a
-            href="#main"
+            href="#about"
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
           >
             <BsPerson size={20} />

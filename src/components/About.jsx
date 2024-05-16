2386e1bd7decb6d5f80ac442ae1f2047aa5aaf38
rinij <rinimani7@gmail.com>
2024-05-15 23:41:54 -0600

    about me section modified

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
+import "../App.css";
 
 const About = () => {
   return (
     <section id="about" className="about--section">
     <div className="about--section--img">
-      <img src="./img/about-me.png" alt="About Me" />
+      <img 
+      src="./img/about-me.jpeg" 
+      alt="About Me" 
+      style={{ width: '150px', height: '200px',borderRadius: '50%' }}
+      />
     </div>
     <div className="hero--section--content--box about--section--box">
       <div className="hero--section--content">
-        <p className="section--title">About</p>
-        <h1 className="skills-section--heading">About Me</h1>
-        <p className="hero--section-description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-          officiis sit debitis omnis harum sed veniam quasi dicta accusamus
-          recusandae? Voluptatem, reprehenderit alias? Eligendi aperiam
-          tempora numquam sint odit optio.
+        {/* <p className="section--title">About</p> */}
+        <h1 className="py-4 text-4xl font-bold text-center text-[#001b5e]">About Me</h1>
+        <p className="text-base sm:text-lg md:text-xl lg:text-xl sm:pb-4 md:pb-4 lg:pb-6">
+          Still thinking what to write here
         </p>
-        <p className="hero--section-description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-          officiis sit debitis omnis harum sed veniam quasi dicta accusamus
-          recusandae?
+        <p className="text-base sm:text-lg md:text-xl lg:text-xl sm:pb-4 md:pb-4 lg:pb-6">
+          Still thinking what to write here
         </p>
       </div>
     </div>

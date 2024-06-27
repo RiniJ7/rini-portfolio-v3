9b7dc7a86e12f3254b4664a8fc05e803e45b3347
rinij <rinimani7@gmail.com>
2024-06-27 17:01:09 -0600

    image property modified

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,13 +5,20 @@ const About = () => {
   return (
     <>
     <section id="about" className="about--section mb-8 sm:mb-16 md:mb-24">
-    <div className="about--section--img sm:mr-8 lg:mr-16">
-      <img 
-      src="./img/aboutMe.png" 
-      alt="About Me" 
- className="w-4 h-4 sm:w-16 sm:h-6 md:w-8 md:h-4 lg:w-4 lg:h-4 "
-      />
-    </div>
+
+
+
+    <div className="about--section--img sm:mr-8 md:mr-8 lg:mr-8">
+  <img 
+    src="./img/aboutMe.png" 
+    alt="About Me" 
+    className="w-18 h-18 sm:w-28 sm:h-28 md:w-36 md:h-36 lg:w-36 lg:h-36"
+  />
+</div>
+
+
+
+
     <div className="hero--section--content--box about--section--box">
       <div className="hero--section--content">
         {/* <p className="section--title">About</p> */}
@@ -32,42 +39,42 @@ Hello! When I'm not deep into coding, you'll probably catch me practicing dance 
 
       <div className="flex justify-center items-center p-6">
   <div className="flex flex-wrap justify-center gap-6 lg:gap-10">
-    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/6 xl:w-1/6">
+    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4">
       <img
         src="./img/1.jpg"
         className="w-full h-32 object-cover rounded-md shadow-lg"
         alt="image1"
       />
     </div>
-    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/6 xl:w-1/6">
+    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4">
       <img
         src="./img/2.jpg"
         className="w-full h-32 object-cover rounded-md shadow-lg"
         alt="image2"
       />
     </div>
-    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/6 xl:w-1/6">
+    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4">
       <img
         src="./img/3.jpg"
         className="w-full h-32 object-cover rounded-md shadow-lg"
         alt="image3"
       />
     </div>
-    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/6 xl:w-1/6">
+    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4">
       <img
         src="./img/4.jpg"
         className="w-full h-32 object-cover rounded-md shadow-lg"
         alt="image4"
       />
     </div>
-    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/6 xl:w-1/6">
+    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4">
       <img
         src="./img/5.jpg"
         className="w-full h-32 object-cover rounded-md shadow-lg"
         alt="image5"
       />
     </div>
-    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/6 xl:w-1/6">
+    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4">
       <img
         src="./img/6.jpg"
         className="w-full h-32 object-cover rounded-md shadow-lg"

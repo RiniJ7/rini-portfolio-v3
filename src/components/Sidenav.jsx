50b7260c102bb1ad848a954915cb1475a85cdd70
rinij <rinimani7@gmail.com>
2024-10-09 14:54:11 -0600

    side nav order modified

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -68,7 +68,10 @@ const Sidenav = () => {
       ) : (
         " "
       )}
+
+      
       <div className="md:block hidden fixed top-[25%] z-10">
+
         <div className="flex flex-col ">
           <a
             href="#main"
@@ -76,17 +79,19 @@ const Sidenav = () => {
           >
             <AiOutlineHome size={20} />
           </a>
-          <a
-            href="#work"
-            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
-          >
-            <GrProjects size={20} />
-          </a>
+
           <a
             href="#projects"
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
           >
             <AiOutlineProject size={20} />
+          </a>
+
+          <a
+            href="#work"
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
+          >
+            <GrProjects size={20} />
           </a>
           <a
             href="#about"
